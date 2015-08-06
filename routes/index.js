89965111ec95fe183dcdf043f5e3bea4a7651623
Dustin Coates <dustin@dcoates.com>
2015-08-06 16:17:22 -0400

    Add route to return a single player with image and _id

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Player = require('../models/player');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/players', function(req, res, next){
+  var player = Player.find({
+    facialHairType: {$exists: false},
+    image: {$exists: true}
+  }, {
+    image: 1, _id: 1
+  }).limit(1).lean().exec(function(err, player){
+    if(err){
+      throw new Error(err);
+    }
+
+    return res.json(player[0]);
+  });
 });
 
 module.exports = router;

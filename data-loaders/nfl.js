f5b198a1627a1ab3d0fc7fc727f38ede350681fd
Dustin Coates <dustin@dcoates.com>
2015-08-06 13:02:10 -0400

    Loop through players for NFL
    There's a 1000 doc bulk insert limit

--- data-loaders/nfl.js
+++ data-loaders/nfl.js
@@ -2,20 +2,34 @@ var fs = require('fs'),
     path = require('path'),
     Player = require('../models/player');
 
+function insertPlayers(players, i, length){
+  var upperBound = i + 1000,
+      slice = players.slice(i, upperBound);
+
+  Player.collection.insert(slice, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      if(upperBound > length){
+        console.log("Players from " + i + " to " + length + " have been created for NFL.");
+        process.exit();
+      } else {
+        console.log("Players from " + i + " to " + upperBound + " have been created for NFL.");
+        insertPlayers(players, upperBound, length);
+      }
+    }
+  });
+
+}
+
 fs.readFile(path.join(__dirname, '../data/nfl.json'), function(err, data){
   if(err){
     console.log(err);
   } else {
-    var players = JSON.parse(data);
+    var players = JSON.parse(data),
+        i = 0,
+        length = players.length;
 
-    Player.collection.insert(players, function(err){
-      if(err){
-        console.log(err);
-      } else {
-        console.log("Players have been created for NFL.");
-      }
-
-      process.exit();
-    });
+    insertPlayers(players, i, length);
   }
 });
